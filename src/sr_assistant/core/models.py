571d1e63c23c6e28a919ca68446a16f8d4ef936a
wfjt <juuso@whiteforest.io>
2025-02-07 01:00:52 +0000

    feat(model): update result model as per schema and types

--- src/sr_assistant/core/models.py
+++ src/sr_assistant/core/models.py
@@ -23,10 +23,11 @@ from datetime import datetime  # noqa: TC003
 
 import sqlalchemy as sa
 import sqlalchemy.dialects.postgresql as sa_pg
-from pydantic import ConfigDict
+from pydantic import ConfigDict, JsonValue
 from sqlmodel import Field, Relationship, SQLModel, String  # type: ignore
 
-from sr_assistant.core.types import ScreeningDecisionType
+from sr_assistant.core.schemas import ExclusionReasons
+from sr_assistant.core.types import ScreeningDecisionType, ScreeningStrategyType
 
 
 def enum_values(enum_class: type[enum.StrEnum]) -> list[str]:
@@ -35,7 +36,7 @@ def enum_values(enum_class: type[enum.StrEnum]) -> list[str]:
 
 
 class SQLModelBase(SQLModel):
-    model_config = ConfigDict( # type: ignore
+    model_config = ConfigDict(  # type: ignore
         from_attributes=True,
         validate_assignment=True,
     )
@@ -45,7 +46,16 @@ class SystematicReview(SQLModelBase, table=True):
     """Systematic review model.
 
     A systematic review project containing the research question, background,
-    and inclusion/exclusion criteria.
+    and inclusion/exclusion criteria. These are combined with search results to
+    generate input to screening chain/agent.
+
+    Notes:
+        - In Python, pubmed_results field links to list of PubMedResult which again
+          have conservative_result and comprehensive_result fields pointing to the
+          abstracts screening results table.
+
+    TODO:
+        - PICO, etc., need to figure out UI updating by agent ...
     """
 
     __tablename__ = "systematic_reviews"  # type: ignore pyright: ignore
@@ -68,7 +78,7 @@ class SystematicReview(SQLModelBase, table=True):
             server_default=sa.func.utcnow(),
             onupdate=sa.func.utcnow(),
             nullable=True,
-        )
+        ),
     )
     background: str | None = Field(
         default=None,
@@ -77,26 +87,21 @@ class SystematicReview(SQLModelBase, table=True):
     research_question: str = Field(
         description="The research question",
     )
-    inclusion_criteria_p: str = Field(
-        description="PICO Population inclusion criteria",
-    )
-    inclusion_criteria_i: str = Field(
-        description="PICO Intervention inclusion criteria",
-    )
-    inclusion_criteria_c: str = Field(
-        description="PICO Comparison inclusion criteria",
-    )
-    inclusion_criteria_o: str = Field(
-        description="PICO Outcome inclusion criteria",
-    )
-    inclusion_criteria: str | None = Field(
-        default=None,
-        description="Other inclusion criteria",
+    # TODO: sort this out, update ui
+    #criteria_framework: str = Field(
+    #    description="The inclusion/exclusion criteria framework",
+    #)
+    #criteria_framework_answers: dict | None = Field(
+    #    default=None,
+    #    description="The answers to the inclusion/exclusion criteria framework",
+    #    sa_column=sa.Column(sa_pg.JSONB(), nullable=True),
+    #)
+    inclusion_criteria: str = Field(
+        description="Inclusion criteria",
     )
     exclusion_criteria: str = Field(
         description="The exclusion criteria for studies",
     )
-
     # One review has many PubMedResults
     pubmed_results: list[PubMedResult] | None = Relationship(
         back_populates="review",
@@ -119,6 +124,16 @@ class PubMedResult(SQLModelBase, table=True):
         sa_column=sa.Column(
             sa.DateTime(timezone=True),
             server_default=sa.func.utcnow(),
+            nullable=True,
+        ),
+    )
+    updated_at: datetime | None = Field(
+        default=None,
+        description="Database generated UTC timestamp",
+        sa_column=sa.Column(
+            sa.DateTime(timezone=True),
+            server_default=sa.func.utcnow(),
+            onupdate=sa.func.utcnow(),
             nullable=True,
         ),
     )
@@ -150,51 +165,64 @@ class PubMedResult(SQLModelBase, table=True):
 
     # One PubMedResult belongs to one SystematicReview
     review_id: uuid.UUID = Field(
-        foreign_key="systematic_reviews.id",
-        ondelete="CASCADE",
-        index=True
+        foreign_key="systematic_reviews.id", ondelete="CASCADE", index=True
     )
     review: SystematicReview = Relationship(
-            back_populates="pubmed_results",
-            cascade_delete=True,
-            sa_relationship_kwargs={"lazy": "selectin"},
+        back_populates="pubmed_results",
+        cascade_delete=True,
+        sa_relationship_kwargs={"lazy": "selectin"},
     )
 
     # One PubMedResult has two ScreenAbstractResults (conservative and comprehensive)
     conservative_result_id: uuid.UUID | None = Field(
-        default=None,
-        foreign_key="screen_abstract_results.id",
-        index=True
+        default=None, foreign_key="screen_abstract_results.id", index=True
     )
     comprehensive_result_id: uuid.UUID | None = Field(
-        default=None,
-        foreign_key="screen_abstract_results.id",
-        index=True
+        default=None, foreign_key="screen_abstract_results.id", index=True
     )
-    conservative_result: ScreenAbstractResult = Relationship(
+    conservative_result: ScreenAbstractResultModel = Relationship(
         sa_relationship_kwargs={
             "foreign_keys": "[PubMedResult.conservative_result_id]",
             "lazy": "selectin",
         }
     )
-    comprehensive_result: ScreenAbstractResult = Relationship(
+    comprehensive_result: ScreenAbstractResultModel = Relationship(
         sa_relationship_kwargs={
             "foreign_keys": "[PubMedResult.comprehensive_result_id]",
             "lazy": "selectin",
         }
     )
 
-
-
-class ScreenAbstractResult(SQLModelBase, table=True):
+# ScreeningResult schema
+# response fields:
+# From model:
+# - decision ScreeningDecisionType
+# - confidence_score float
+# - rationale str
+# - extracted_quotes list[str]
+# - exclusion_reason_categories ExclusionReasons
+# Injected:
+# - id uuid.UUID (run id)
+# - review_id uuid.UUID
+# - pubmed_result_id uuid.UUID
+# - trace_id uuid.UUID
+# - screening_strategy ScreeningStrategyType (comprehensive/conservative)
+# - model_name str
+# - start_time datetime (UTC)
+# - end_time datetime (UTC)
+# - response_metadata dict[str, JsonValue], JSONB in postgres
+class ScreenAbstractResultModel(SQLModelBase, table=True):
     """Abstract screening decision model.
 
     Tracks the screening decisions for each paper in the review.
     """
 
-    __tablename__ = "screen_abstract_results" # type: ignore pyright: ignore
+    __tablename__ = "screen_abstract_results"  # type: ignore pyright: ignore
 
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    id: uuid.UUID | None = Field(
+        default_factory=uuid.uuid4, primary_key=True,
+        description="Should be the run_id of the child-child `langsmith.RunTree`, populated by the chain `on_end` listener."
+    )
     created_at: datetime | None = Field(
         default=None,
         description="Database generated UTC timestamp",
@@ -212,7 +240,7 @@ class ScreenAbstractResult(SQLModelBase, table=True):
             server_default=sa.func.utcnow(),
             onupdate=sa.func.utcnow(),
             nullable=True,
-        )
+        ),
     )
     # Screening decision fields
     decision: ScreeningDecisionType = Field(
@@ -233,19 +261,47 @@ class ScreenAbstractResult(SQLModelBase, table=True):
     extracted_quotes: list[str] | None = Field(
         default=None,
         description="Supporting quotes from the title/abstract. Can be omitted if uncertain.",
-        sa_column=sa.Column(sa_pg.ARRAY(String())),
+        sa_column=sa.Column(sa_pg.ARRAY(String()), nullable=True),
     )
-    exclusion_reason_categories: list[str] | None = Field(
+    exclusion_reason_categories: ExclusionReasons | None = Field(
         default=None,
-        description="The PRISMA exclusion reason categories for the decision. This complements the 'rationale' field. TODO: how to type?",
-        sa_column=sa.Column(sa_pg.ARRAY(String())),
+        description="The PRISMA exclusion reason categories for the decision. This complements the 'rationale' field.",
+        sa_column=sa.Column(sa_pg.JSONB(), nullable=True),
+    )
+    # Injected fields
+    # Screening decision fields
+    start_time: datetime | None = Field(
+        description="Chain/graph invocation start time.",
+        sa_column=sa.Column(
+            sa.DateTime(timezone=True),
+            nullable=True,
+        ),
+    )
+    end_time: datetime | None = Field(
+        description="Chain/graph invocation end time.",
+        sa_column=sa.Column(
+            sa.DateTime(timezone=True),
+            nullable=True,
+        ),
+    )
+    screening_strategy: ScreeningStrategyType = Field(
+        description="Currently either 'compherensive' or 'conservative'. Maps to kind of prompt used.",
+        sa_column=sa.Column(
+            type_=sa_pg.ENUM(ScreeningStrategyType, values_callable=enum_values),
+            nullable=False,
+        ),
+    )
+    model_name: str = Field(
+        description="The LangChain model name that generated this response.",
+    )
+    response_metadata: dict[str, JsonValue] | None = Field(
+        default=None,
+        description="Metadata from the chain/graph invocation.",
+        sa_column=sa.Column(sa_pg.JSONB(), nullable=True),
     )
 
     # Screening Result is associated with one review
-    review_id: uuid.UUID = Field(
-        foreign_key="systematic_reviews.id",
-        index=True
-    )
+    review_id: uuid.UUID = Field(foreign_key="systematic_reviews.id", index=True)
     # Each ScreenAbstractResult belongs to one Review
     review: SystematicReview = Relationship(
         back_populates="screen_abstract_results",
@@ -255,28 +311,10 @@ class ScreenAbstractResult(SQLModelBase, table=True):
     pubmed_result_id: uuid.UUID = Field(
         description="The PubMed result being screened",
         foreign_key="pubmed_results.id",
-        index=True
+        index=True,
     )
-    # Each ScreenAbstractResult belongs to one PubMedResult
+    # Each ScreenAbstractResult belongs to one PubMedResult. Relationship other way is
+    # configured in PubMedResult.
     pubmed_result: PubMedResult = Relationship(
         sa_relationship_kwargs={"lazy": "selectin"},
     )
-
-
-class UuidTest(SQLModelBase, table=True):
-    """Test model for UUID handling."""
-
-    __tablename__ = "uuid_test"  # type: ignore pyright: ignore
-
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    created_at: datetime | None = Field(
-        default=None,
-        description="Database generated UTC timestamp",
-        title="Created At",
-        sa_column=sa.Column(
-            sa.DateTime(timezone=True),
-            server_default=sa.func.utcnow(),
-            nullable=True,
-        ),
-    )
-    data: str = Field(...)
